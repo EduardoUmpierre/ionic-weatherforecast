ae78152b8490fe2712927259983f7d2b0458dd6c
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2017-08-28 22:04:52 -0300

    3 days weather forecast added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public weather: any[];
+  public city: string;
+  public country: string;    
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private http: Http) {
 
+  }
+
+  ngOnInit() {
+      this.http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Porto%20Alegre,Brazil&cnt=3&units=metric&appid=94cd6c40f0df1baa9b7db667ba255def')
+          .map((res: Response) => {
+              let body = res.json();
+
+              return body || {};
+          })
+          .subscribe(weather => {
+              console.log(weather);
+              this.city = weather.city.name;
+              this.country = weather.city.country;
+
+              this.weather = weather.list;
+          });
   }
 
 }
