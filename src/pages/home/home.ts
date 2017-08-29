5e30111672829e55726f0a8cbc9eb86e2f32b549
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2017-08-29 15:06:25 -0300

    Home page content added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,35 +1,27 @@
 import { Component } from '@angular/core';
-import { Http, Response } from '@angular/http';
 import { NavController } from 'ionic-angular';
-import 'rxjs/add/operator/map';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-  public weather: any[];
-  public city: string;
-  public country: string;    
+    public weather:any[];
+    public city:string;
+    public country:string;
 
-  constructor(public navCtrl: NavController, private http: Http) {
+    constructor(public navCtrl:NavController, private weatherProvider:WeatherProvider) {
 
-  }
+    }
 
-  ngOnInit() {
-      this.http.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Porto%20Alegre,Brazil&cnt=3&units=metric&appid=94cd6c40f0df1baa9b7db667ba255def')
-          .map((res: Response) => {
-              let body = res.json();
-
-              return body || {};
-          })
-          .subscribe(weather => {
-              console.log(weather);
-              this.city = weather.city.name;
-              this.country = weather.city.country;
-
-              this.weather = weather.list;
-          });
-  }
-
+    ngOnInit() {
+        this.weatherProvider
+            .builder('weather', 'Porto Alegre,Brazil', null)
+            .getWeatherData()
+            .then((res) => {
+                this.weather = res;
+                console.log(res);
+            });
+    }
 }
